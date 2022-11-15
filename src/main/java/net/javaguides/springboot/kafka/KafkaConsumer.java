6845ea9cd1a5bf892cb5a02f473541d22e1c5d98
Howin Ho <ho1ho@yahoo.com>
2022-11-15 08:58:03 -0500

    kafka consumer

---
+++ src/main/java/net/javaguides/springboot/kafka/KafkaConsumer.java
@@ -0,0 +1,19 @@
+package net.javaguides.springboot.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import static net.javaguides.springboot.SpringbootKafkaTutorialApplication.TOPIC;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = TOPIC, groupId = "group_id")
+    public void consume(String message) {
+        LOGGER.info(String.format("Message received -> %s", message));
+    }
+}
